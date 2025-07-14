cd7cdf8532ffa8b700559ea2fb4b06a2d6e6e784
jlope384 <lop23415@uvg.edu.gt>
2025-07-13 22:09:08 -0600

    Polígono 3 rojo con orilla blanca

--- src/main.rs
+++ src/main.rs
@@ -126,34 +126,15 @@ fn main() {
 
     let mut image = Image::gen_image_color(width, height, Color::WHITE);
 
-    let polygon1 = [
-        (165, 380), (185, 360), (180, 330), (207, 345), (233, 330),
-        (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)
-    ];
 
-    let polygon2: [(i32, i32); 4] = [
-        (321, 335), (288, 286), (339, 251), (374, 302)
-    ];
 
     let polygon3 = [
         (377, 249), (411, 197), (436, 249)
     ];
 
-    let polygon4 = [
-        (413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37),
-        (660, 52), (750, 145), (761, 179), (672, 192), (659, 214), (615, 214),
-        (632, 230), (580, 230), (597, 215), (552, 214), (517, 144), (466, 180)
-    ];
 
-    let polygon5 = [
-        (682, 175), (708, 120), (735, 148), (739, 170)
-    ];
 
-    fill_polygon(&mut image, &polygon1, Color::YELLOW, Color::WHITE, &[]);
-    fill_polygon(&mut image, &polygon2, Color::BLUE, Color::WHITE, &[]);
     fill_polygon(&mut image, &polygon3, Color::RED, Color::WHITE, &[]);
-    fill_polygon(&mut image, &polygon4, Color::GREEN, Color::WHITE, &[&polygon5]);
-    fill_polygon(&mut image, &polygon5, Color::WHITE, Color::WHITE, &[]); 
 
     image.flip_vertical(); // Las imagenes en Raylib están al revés verticalmente
 
