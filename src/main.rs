de202403f8f461beef2505e148a68f16743f50ca
jlope384 <lop23415@uvg.edu.gt>
2025-07-13 22:17:44 -0600

    xd

--- src/main.rs
+++ src/main.rs
@@ -117,6 +117,7 @@ fn point_in_polygon(point: (i32, i32), polygon: &[(i32, i32)]) -> bool {
 }
 
 
+
 fn main() {
     let (width, height) = (800, 450);
     let (mut rl, thread) = raylib::init()
@@ -126,17 +127,38 @@ fn main() {
 
     let mut image = Image::gen_image_color(width, height, Color::WHITE);
 
+    let polygon1 = [
+        (165, 380), (185, 360), (180, 330), (207, 345), (233, 330),
+        (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)
+    ];
 
+    let polygon2 = [
+        (321, 335), (288, 286), (339, 251), (374, 302)
+    ];
 
+    let polygon3 = [
+        (377, 249), (411, 197), (436, 249)
+    ];
 
+    let polygon4 = [
+        (413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37),
+        (660, 52), (750, 145), (761, 179), (672, 192), (659, 214), (615, 214),
+        (632, 230), (580, 230), (597, 215), (552, 214), (517, 144), (466, 180)
+    ];
+
+    let polygon5 = [
+        (682, 175), (708, 120), (735, 148), (739, 170)
+    ];
 
     fill_polygon(&mut image, &polygon1, Color::YELLOW, Color::WHITE, &[]);
+    fill_polygon(&mut image, &polygon2, Color::BLUE, Color::WHITE, &[]);
+    fill_polygon(&mut image, &polygon3, Color::RED, Color::WHITE, &[]);
     fill_polygon(&mut image, &polygon4, Color::GREEN, Color::WHITE, &[&polygon5]);
-    fill_polygon(&mut image, &polygon5, Color::WHITE, Color::WHITE, &[]); 
+    fill_polygon(&mut image, &polygon5, Color::WHITE, Color::WHITE, &[]); // aquí se pinta de blanco
 
-    image.flip_vertical(); // Las imagenes en Raylib están al revés verticalmente
+    image.flip_vertical(); // invierte el eje Y para visualización más natural
 
-    image.export_image("out.bmp");
+    image.export_image("poligonos_rellenos.png");
 
     let texture = rl.load_texture_from_image(&thread, &image).unwrap();
 
