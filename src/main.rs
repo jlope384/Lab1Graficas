3fbc7d78407462a469efbf7bf099c3d88d38f078
jlope384 <lop23415@uvg.edu.gt>
2025-07-13 22:09:27 -0600

    Polígono 4 verde con agujero

--- src/main.rs
+++ src/main.rs
@@ -126,18 +126,7 @@ fn main() {
 
     let mut image = Image::gen_image_color(width, height, Color::WHITE);
 
-    let polygon1 = [
-        (165, 380), (185, 360), (180, 330), (207, 345), (233, 330),
-        (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)
-    ];
 
-    let polygon2: [(i32, i32); 4] = [
-        (321, 335), (288, 286), (339, 251), (374, 302)
-    ];
-
-    let polygon3 = [
-        (377, 249), (411, 197), (436, 249)
-    ];
 
     let polygon4 = [
         (413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37),
@@ -150,8 +139,6 @@ fn main() {
     ];
 
     fill_polygon(&mut image, &polygon1, Color::YELLOW, Color::WHITE, &[]);
-    fill_polygon(&mut image, &polygon2, Color::BLUE, Color::WHITE, &[]);
-    fill_polygon(&mut image, &polygon3, Color::RED, Color::WHITE, &[]);
     fill_polygon(&mut image, &polygon4, Color::GREEN, Color::WHITE, &[&polygon5]);
     fill_polygon(&mut image, &polygon5, Color::WHITE, Color::WHITE, &[]); 
 
